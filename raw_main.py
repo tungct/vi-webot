ff83322339fef8681ba1a9fda1828a16359283eb
Cao-Tung <caotungjs@gmail.com>
2018-05-07 10:21:22 +0700

    init

---
+++ raw_main.py
@@ -0,0 +1,7 @@
+from  web import raw_main
+# from adapter.ner import train
+# from adapter.greeting import train
+# from adapter.intend import train
+# train.train()
+# train.main()
+raw_main.main()
